b329c4e051eabfcfcac2baa9b2c903e25de19d9d
dhilipsiva <dhilipsiva@gmail.com>
2016-02-23 08:21:23 +0530

    Add the stupid script

---
+++ index.js
@@ -0,0 +1,58 @@
+/*
+ * app.js
+ * Copyright (C) 2014 dhilipsiva <dhilipsiva@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+var parseArgs = require('minimist')
+  , port = 8008
+  , io = require('socket.io')(port)
+  , redis = require('redis')
+  , argv = parseArgs(process.argv)
+  , redisPort = argv.port || 6379
+  , redisHost = argv.host || 'localhost'
+  , client = redis.createClient(parseInt(redisPort), redisHost);
+
+console.log('server listens on port ' + port);
+
+io.sockets.on('connection', function (socket) {
+
+  socket.on('subscribe', function(data) {
+    socket.join(data.room);
+    console.log("User joined the room: ", data);
+  })
+
+  socket.on('unsubscribe', function(data) {
+    socket.leave(data.room);
+    console.log("User left the room: ", data);
+  })
+
+  socket.on('disconnect', function(data) {
+    socket.leave(data.room);
+    console.log("User quit the room: ", data);
+  })
+
+});
+
+client.on("message", function (channel, message) {
+
+  console.log("channel:%s - message:%s",channel, message);
+
+  data = JSON.parse(message);
+  data.rooms.forEach(function(room){
+    io.sockets.in(room).emit(data.event, data.data);
+  });
+
+  console.log("Got a new message: ", data.rooms);
+
+});
+
+client.subscribe("notify");
+
+/*
+ * Usage on client side
+ *
+ * socket.send("subscribe", { room: "user uuid" });
+ *
+ */
